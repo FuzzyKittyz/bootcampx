ddbc4553fa3a738c77b66796da4e17b80c762be2
FuzzyKittyz <topher.a@hotmail.com>
2022-10-26 19:46:16 -0600

    add parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,15 +1,20 @@
 const { Pool } = require('pg');
 
 const pool = new Pool();
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+
+let queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+ORDER BY teacher;`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
